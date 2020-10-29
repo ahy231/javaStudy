bf4cd75b3f16d493aee298a86bad0c2bffee4edc
ahy231 <zhongnanhai231@126.com>
2020-10-29 14:14:28 +0800

    update HTML DOM

---
+++ html-DOM/src/main/js/test.js
@@ -0,0 +1,8 @@
+/*
+ * @Copyright: free for non-commercial usage
+ * @Author: ahy231
+ * @Date: 2020-10-29 14:04:21
+ * @LastEditor: ahy231
+ * @LastEditTime: 2020-10-29 14:04:38
+ */
+alert("this is test.js");
