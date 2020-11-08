5c53f8edb011464558b2dd92ded634a36ec48e95
ahy231 <zhongnanhai231@126.com>
2020-11-08 09:34:05 +0800

    update Vue

--- Vue/src/main/js/gareen.js
+++ Vue/src/main/js/gareen.js
@@ -3,16 +3,25 @@
  * @Author: ahy231
  * @Date: 2020-11-04 16:43:28
  * @LastEditor: ahy231
- * @LastEditTime: 2020-11-06 17:13:22
+ * @LastEditTime: 2020-11-08 09:31:11
  */
+
+//首字母大写
+Vue.filter('capitalize', function (val) {
+    if (!val) return '';
+    val = val.toString();
+    return val.charAt(0).toUpperCase() + val.substring(1);
+})
+
+Vue.filter('capitalizeLastLetter', function (val) {
+    if (!val) return '';
+    val = val.toString();
+    return val.substring(0, val.length - 1) + val.charAt(val.length - 1).toUpperCase();
+})
+
 new Vue({
-    el: '#div1',
+    el: "#div1",
     data: {
-        number: 0
-    },
-    methods: {
-        drawLot: function () {
-            this.number = Math.random() * 90 + 10;
-        }
+        data: ''
     }
 })
