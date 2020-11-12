ac6965c7e01759e9795da33ed9db4fe3153b0c35
unknown <zhongnanhai231@126.com>
2020-11-12 22:27:55 +0800

    add projects

---
+++ Vue/src/main/js/index.js
@@ -0,0 +1,16 @@
+/*
+ * @Copyright: free for non-commercial usage
+ * @Author: ahy231
+ * @Date: 2020-11-04 16:43:28
+ * @LastEditor: ahy231
+ * @LastEditTime: 2020-11-08 19:21:08
+ */
+
+new Vue({
+    el: "#div1",
+    components:{
+        'product':{
+            template:'<div class="product">MAXFEEL休闲男士手包真皮手拿包大容量信封包手抓包夹包软韩版潮</div>'
+        }
+    }
+})
