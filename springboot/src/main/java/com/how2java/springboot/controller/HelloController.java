4b21987b5c614dc6f1f2576d1aacd7c3c903177f
unknown <zhongnanhai231@126.com>
2020-11-29 19:12:57 +0800

    update springmvc, springboot

--- springboot/src/main/java/com/how2java/springboot/controller/HelloController.java
+++ springboot/src/main/java/com/how2java/springboot/controller/HelloController.java
@@ -1,17 +1,19 @@
-package com.how2java.springboot.web;
+package com.how2java.springboot.controller;
 import java.text.DateFormat;
 import java.util.Date;
 
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HelloController {
+public class HelloController extends SpringBootServletInitializer {
 
     @RequestMapping("/hello")
-    public String hello(Model m) {
+    public String hello(Model m) throws Exception {
         m.addAttribute("now", DateFormat.getDateTimeInstance().format(new Date()));
-        return "hello";
+        throw new Exception();
+//        return "hello";
     }
 }
---
+++ springboot/src/main/java/com/how2java/springboot/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.how2java.springboot.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ModelAndView defaultErrorHandler(HttpServletRequest request, Exception e) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("exception", e);
+        modelAndView.addObject("url", request.getRequestURL());
+        modelAndView.setViewName("errorPage");
+        return modelAndView;
+    }
+}
