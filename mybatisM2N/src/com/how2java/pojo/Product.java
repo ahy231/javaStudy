10208c514337b5abb2a4c4595131c4bdf3e03bd2
unknown <zhongnanhai231@126.com>
2020-12-06 11:36:47 +0800

    update mybatis

---
+++ mybatisM2N/src/com/how2java/pojo/Product.java
@@ -0,0 +1,40 @@
+package com.how2java.pojo;
+
+public class Product {
+    public int id;
+    public String name;
+    public double price;
+    public int cid;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+}
