c78ad73979caabe60887fbeb1e8850e3dd72f8f5
unknown <zhongnanhai231@126.com>
2020-12-10 13:33:26 +0800

    update mybatis

--- mybatisAnnoCRUD/src/com/how2java/mapper/CategoryMapper.java
+++ mybatisAnnoCRUD/src/com/how2java/mapper/CategoryMapper.java
@@ -1,4 +1,27 @@
 package com.how2java.mapper;
 
-public class CategoryMapper {
+import com.how2java.pojo.Category;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+public interface CategoryMapper {
+
+    @Insert("insert into category_ (name) values (#{name})")
+    public int add(Category category);
+
+    @Delete("delete from category_ where id= #{id}")
+    public void delete(int id);
+
+    @Select("select * from category_ where id= #{id}")
+    public Category get(int id);
+
+    @Update("update category_ set name= #{name} where id= #{id}")
+    public int update(Category category);
+
+    @Select("select * from category_")
+    public List<Category> list();
 }
